e9aedd92c51a8a829e0fda9a9414c92ea05c2380
Andres Martinez <andres.martinez@proveeduria.unam.mx>
2024-06-24 17:53:01 -0600

    Initial Commit

---
+++ Modulo 9 Practica 1/Modulo_9_Practica_1App.swift
@@ -0,0 +1,17 @@
+//
+//  Modulo_9_Practica_1App.swift
+//  Modulo 9 Practica 1
+//
+//  Created by Desarrollo on 24/06/24.
+//
+
+import SwiftUI
+
+@main
+struct Modulo_9_Practica_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
